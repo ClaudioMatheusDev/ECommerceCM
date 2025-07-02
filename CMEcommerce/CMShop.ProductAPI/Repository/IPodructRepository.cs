0feb66bc9461c8f2616d3f1529ad8705e3e5cbc1
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-07-02 20:27:51 -0300

    ❇️ feat: Definindo as propriedades da interface IProductRepository

--- CMEcommerce/CMShop.ProductAPI/Repository/IPodructRepository.cs
+++ CMEcommerce/CMShop.ProductAPI/Repository/IPodructRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace CMShop.ProductAPI.Repository
+﻿using CMShop.ProductAPI.Data.ValueObjects;
+
+namespace CMShop.ProductAPI.Repository
 {
     public interface IPodructRepository
     {
-
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
