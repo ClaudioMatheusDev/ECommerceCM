d6feb3ca588e4af41bb004f2165c5e0bcedf3647
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-07-02 21:27:45 -0300

    ☑️ feat: Implementando as operacoes POST, PUT e DELETE no CONTROLLER PRODUCT

--- CMEcommerce/CMShop.ProductAPI/Repository/ProductRepository.cs
+++ CMEcommerce/CMShop.ProductAPI/Repository/ProductRepository.cs
@@ -46,17 +46,20 @@ namespace CMShop.ProductAPI.Repository
         {
             try
             {
-                Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
-                if (product != null) ;
-                return false;
+                Product product = await _context.Products
+                    .Where(p => p.Id == id)
+                    .FirstOrDefaultAsync();
+
+                if (product == null)
+                    return false; 
+
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
-                return true;
 
+                return true; 
             }
             catch (Exception)
             {
-
                 return false;
             }
         }
